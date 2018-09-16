b21e04b965ea84b221f6ff45c6a3a3b54208e056
Andrey Denisenko <francescogacczi@gmail.com>
2018-09-16 11:28:19 +0300

    ch2

--- 13-5/Program.cs
+++ 13-5/Program.cs
@@ -8,8 +8,27 @@ namespace _13_5
 {
     class Program
     {
+        public static string getString()
+        {
+            string str = Console.ReadLine();
+            return str;
+        }
+        public static int getInt()
+        {
+            string str = getString();
+            return Int32.Parse(str);
+        }
         static void Main(string[] args)
         {
+            Console.Write("enter x：");
+            int x = getInt();
+            Console.Write("enter y：");
+            int y = getInt();
+            Chess chess = new Chess(x, y);
+            // chess.disPlayChessBoard();
+            chess.knightTour();
+
+            Console.ReadLine();
         }
     }
 }
